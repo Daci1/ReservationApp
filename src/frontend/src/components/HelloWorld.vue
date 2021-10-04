18546e1b5045f3f8d365fd08ac4b967f955413bc
Daci1 <dacian.barbu28@gmail.com>
2021-10-04 23:25:56 +0300

    Created the frontend project and added the necessary pom.xml configuration for the jar

---
+++ src/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Welcome to the page</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      msg: ' '
+    }
+  },
+  mounted() {
+    
+  }
+
+}
+</script>
+
